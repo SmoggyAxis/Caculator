53a1e3b9105af83daf07cc1f3914d34291da3b33
SmoggyAxis <89107958+SmoggyAxis@users.noreply.github.com>
2022-10-24 02:37:22 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int x;
+    int z;
+    Console.WriteLine ("Calculator - give me two numbers!");
+    x = Convert.ToInt32(Console.ReadLine());
+    z = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(x+z);
+    
+  
+  }
+}
